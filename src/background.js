76e725e6ff9aa14b8ac6ca36b3c16b6388aa4177
zzg <975129758@qq.com>
2021-12-04 00:21:12 +0800

    1

--- src/background.js
+++ src/background.js
@@ -16,7 +16,6 @@ const os = require('os')
 global.constDict = {
   bookPath:path.join(os.homedir(),'cloud_reader_desktop','books'),
   serverHost:'http://110.42.188.51',
-  // serverHost:'http://localhost:18081',
 };
 // 初始化文件目录
 if(!fs.existsSync(constDict.bookPath)){
@@ -34,11 +33,8 @@ if(!fs.existsSync(constDict.bookPath)){
 
 
 
-
-// import installExtension, { VUEJS3_DEVTOOLS } from 'electron-devtools-installer'
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
-// Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
   { scheme: 'app', privileges: { secure: true, standard: true } }
 ])
@@ -54,7 +50,6 @@ ipcMain.on('download-book',function(event,id,bookName){
 
 ipcMain.on('open-pdf', function(event, pdfUrl,pageNum,bookId) {
   openPdfWin(pdfUrl,pageNum,bookId)
-  // event.sender.send('asynchronous-reply', 'pong');
 });
 
 function downloadFile(uri,filename,callback){
@@ -76,7 +71,6 @@ function openPdfWin(bookName,pageNum,bookId){
   if(isDevelopment){
     pdfWin.loadURL('file://' + __dirname + '/bundled/pdfviewer/web/viewer.html?file='+
     encodeURIComponent('http://localhost:9090/getPdf?pdfName='+bookName)+'#page='+pageNum+'&bookId='+bookId );
-    // encodeURIComponent('http://localhost:9090/getPdf?pdfName='+pdfName+'#'+pageNum) );
   }else{
     pdfWin.loadURL('file://' + __dirname + '/pdfviewer/web/viewer.html?file='+
     encodeURIComponent('http://localhost:9090/getPdf?pdfName='+bookName)+'#page='+pageNum+'&bookId='+bookId );
@@ -107,14 +101,14 @@ function createHttpServer(){
     }
     else if(pathname == '/updatePage'){
       // get获取数据
-      const {currPage,bookId} = url.parse(req.url,true).query
+      const {currPage,bookId,pagesCount} = url.parse(req.url,true).query
 
       request({
         url: constDict.serverHost+'/book/person/updateCurrPage',
         method: "POST",
         json: true,
         headers: {'Authorization': store.get('token')},
-        body: {currPage: currPage,bookId:bookId}
+        body: {currPage: currPage,bookId:bookId,pagesCount:pagesCount}
       }, function(error, response, body) {
        
         res.end('updatePage')
@@ -129,66 +123,44 @@ function createHttpServer(){
 
 async function createWindow() {
   
-  // Create the browser window.
   const win = new BrowserWindow({
     width: 1100,
     height: 707,
     webPreferences: {
       
-      // Use pluginOptions.nodeIntegration, leave this alone
-      // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
-      // nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
-      // contextIsolation: !process.env.ELECTRON_NODE_INTEGRATION,
       nodeIntegration: true, // is default value after Electron v5
       contextIsolation: false, // protect against prototype pollution
       enableRemoteModule: true, //this must be true
       webSecurity: false,
-      // preload: path.join(__dirname, 'preload.js'),
       // 插件支持
       plugins :true,
     }
   })
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
-    //  todo 加载pdf
-    // await win.loadURL(path.join(__dirname, 'file/a.pdf#page=100'))
+
     await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
 
-    // const view = new BrowserView()
-    // win.setBrowserView(view)
-    // view.setBounds({ x: 300, y: 0, width: 500, height: 600 })
-    // view.webContents.loadURL(path.join(__dirname, 'file/a.pdf#page=100'))
-
-
-    // if (!process.env.IS_TEST) win.webContents.openDevTools()
   } else {
     createProtocol('app')
-    // Load the index.html when not in development
     win.loadURL('app://./index.html')
   }
 }
 
-// Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
+
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) createWindow()
 })
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+
 app.on('ready', async () => {
   if (isDevelopment && !process.env.IS_TEST) {
-    // Install Vue Devtools
     try {
       await installExtension(VUEJS3_DEVTOOLS)
     } catch (e) {
@@ -205,7 +177,6 @@ app.on('ready', async () => {
   createHttpServer()
 })
 
-// Exit cleanly on request from parent process in development mode.
 if (isDevelopment) {
   if (process.platform === 'win32') {
     process.on('message', (data) => {
--- src/pages/BookListPage.vue
+++ src/pages/BookListPage.vue
@@ -40,7 +40,7 @@
 
         <div style="height:500px">
             <a-row >
-                <a-card  hoverable style="width: 150px;margin:5px" v-for="(val,key) in books" :key="key" >
+                <a-card @click="openDetail(val)" hoverable style="width: 150px;margin:5px" v-for="(val,key) in books" :key="key" >
                     <img
                     style="width:100%"
                     slot="cover"
@@ -49,13 +49,21 @@
                     <a-card-meta :title="val.bookName">
                         <template #description>
                             <CloudDownloadOutlined v-if="!val.isDown" :id="`download${val.id}`" @click="downloadBook(val.id,val.fileName)"/>
-                            <a-button v-if="val.isDown" style="float:right" type="primary" size="small" @click="openPdf(val)">阅读</a-button>
+                            <a-button v-if="val.isDown" style="float:right" type="primary" size="small" @click.stop="openPdf(val)">阅读</a-button>
                         </template>
                     </a-card-meta>
                 
                 </a-card>
             </a-row>
         </div>
+
+        <a-modal v-model:visible="visible" :title="detail.title" @ok="handleOk">
+            上次阅读：{{detail.lastReadTime}}<br>
+            阅读进度：{{detail.readProgress}}
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+        </a-modal>
 
         <a-pagination v-model:current="current" :total="total" show-less-items />
 
@@ -84,7 +92,15 @@ export default {
             },
             uploadUrl:this.$serverHost+'/book/upload',
             current:1,
-            total:0
+            total:0,
+
+            visible:false,
+
+            detail:{
+                title:'title',
+                lastReadTime:'1900-01-01 00:00:00',
+                readProgress:'未阅读'
+            }
         }
     },
     watch: {
@@ -93,6 +109,15 @@ export default {
         },
     },
     methods:{
+        openDetail(book){
+            this.detail.title = book.bookName
+            this.detail.lastReadTime = book.lastReadTime?this.timeFormat(book.lastReadTime.split('T')[0]+' '+book.lastReadTime.split('T')[1]):'未阅读'
+            this.detail.readProgress = book.pageNum&&book.currPage?book.currPage+'/'+book.pageNum:'未阅读'
+            this.visible = true
+        },
+        handleOk(e){
+            this.visible = false;
+        },
         uploadHandle(info){
             if (info.file.status !== 'uploading') {
                 console.log(info.file, info.fileList);
@@ -180,6 +205,7 @@ export default {
             })
         },
         openPdf(file){
+            
 
              this.$axios.get(this.$serverHost+'/book/person/personBook/'+file.bookId,
             {
@@ -193,6 +219,43 @@ export default {
                 message.error("获取当前页码错误");
             })
                   
+        },
+        timeFormat(time) {
+            var nowTime = new Date();
+            var day = nowTime.getDate();
+            var hours = parseInt(nowTime.getHours());
+            var minutes = nowTime.getMinutes();
+            // 开始分解付入的时间
+            var timeday = time.substring(8, 10);
+            var timehours = parseInt(time.substring(11, 13));
+            var timeminutes = time.substring(14, 16);
+            var d_day = Math.abs(day - timeday);
+            var d_hours = hours - timehours;
+            var d_minutes = Math.abs(minutes - timeminutes);
+            if (d_day <= 1) {
+                switch (d_day) {
+                    case 0:
+                        if (d_hours == 0 && d_minutes > 0) {
+                            return d_minutes + '分钟前';
+                        } else if (d_hours == 0 && d_minutes == 0) {
+                            return '1分钟前';
+                        } else {
+                            return d_hours + '小时前';
+                        }
+                        break;
+                    case 1:
+                        if (d_hours < 0) {
+                            return (24 + d_hours) + '小时前';
+                        } else {
+                            return d_day + '天前';
+                        }
+                        break;
+                }
+            } else if (d_day > 1 && d_day < 10) {
+                return d_day + '天前';
+            } else {
+                return time;
+            }
         },
     },
     created(){
